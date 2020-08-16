bc0f0b36a95f841954ba35994dafa2aec1576a2d
marcomeneghini <marco.meneghini80@gmail.com>
2020-08-16 17:21:51 +0100

    Added Index Builder and UT

---
+++ ConstructionLine.CodingChallenge/IObjectIndexBuilder.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public interface IObjectIndexBuilder<T>
+    {
+        List<IObjectIndex<T>> BuildIndexes(List<T> objects);
+    }
+}
