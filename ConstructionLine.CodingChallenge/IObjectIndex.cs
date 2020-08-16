07d5c77e2faf53dd2665f3acdd30de6bff023093
marcomeneghini <marco.meneghini80@gmail.com>
2020-08-16 16:55:28 +0100

    Added ShirSizeIndex and UT

---
+++ ConstructionLine.CodingChallenge/IObjectIndex.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public interface IObjectIndex<T>
+    {
+        IEnumerable<T> Find(SearchOptions searchOptions);
+    }
+}
---
+++ ConstructionLine.CodingChallenge/ShirtSizeIndex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public class ShirtSizeIndex : IObjectIndex<Shirt>
+    {
+        private readonly ILookup<Size, Shirt> _shirtSizes;
+        public ShirtSizeIndex(List<Shirt> shirts)
+        {
+            _shirtSizes = shirts.ToLookup(x => x.Size, x => x);
+        }
+
+        public IEnumerable<Shirt> Find(SearchOptions searchOptions)
+        {
+            if (!searchOptions.Sizes.Any())
+                return _shirtSizes.SelectMany(shirt => shirt);
+            return _shirtSizes
+                .Where(x => searchOptions.Sizes.Contains(x.Key))
+                .SelectMany(shirt => shirt);
+        }
+    }
+}
