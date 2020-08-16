8088b0d12262c1e6950d9d213de044a08959a7e9
marcomeneghini <marco.meneghini80@gmail.com>
2020-08-16 17:45:48 +0100

    Added Scanner and UT

---
+++ ConstructionLine.CodingChallenge/IObjectIndexScanner.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public interface IObjectIndexScanner<T> // TODO: ,K> where K are the specific search criteria 
+    {
+        // TODO: make SearchOptions Generic 
+        List<T> GetObjects(IEnumerable<IObjectIndex<T>> indexes, SearchOptions searchOptions);
+    }
+}
---
+++ ConstructionLine.CodingChallenge/ShirtIndexScanner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConstructionLine.CodingChallenge
+{
+    public class ShirtIndexScanner : IObjectIndexScanner<Shirt>
+    {
+        public List<Shirt> GetObjects(IEnumerable<IObjectIndex<Shirt>> indexes, SearchOptions searchOptions)
+        {
+            List<Shirt> shirts = new List<Shirt>();
+            foreach (var index in indexes)
+            {
+                var shirtsPerIndex = index.Find(searchOptions);
+                if (!shirtsPerIndex.Any())                
+                    continue;
+                
+                if (shirts.Any())
+                {
+                    shirts = shirts.Intersect(shirtsPerIndex).ToList();
+                }
+                else
+                {
+                    shirts = shirtsPerIndex.ToList();
+                }
+            }
+
+            return shirts;
+        }
+    }
+}
